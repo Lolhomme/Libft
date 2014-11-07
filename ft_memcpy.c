1d9a761782059003875c6574ee19c4b99ad1fa32
Laulom anthony <anthonylaulom@gmail.com>
2014-11-07 19:01:17 +0100

    memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/07 18:49:57 by alaulom           #+#    #+#             */
+/*   Updated: 2014/11/07 18:58:01 by alaulom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char			*ptrdst;
+	const unsigned char		*ptrsrc;
+
+	ptrdst = (unsigned char *)dst;
+	ptrsrc = (const unsigned char *)src;
+	while (n--)
+		*ptrdst++ = *ptrsrc++;
+	return (dst);
+}

262235500ed06b24b0210420e650a6aa7dbacb47
Laulom anthony <anthonylaulom@gmail.com>
2014-11-07 11:36:29 +0100

    fuck patate

--- libft.h
+++ libft.h
@@ -11,5 +11,7 @@ int			ft_strcmp(const char *s1, const char *s2)
 char		ft_strcat(char *s1, const char *s2)
 char 		*ft_strdup(const char *s1)
 char		*ft_strchr(const char *s, int c)
+char 		*ft_strrchr(const char *s, int c)
+char		*ft_strncat(char *s1, const char *s2, size_t n)
 
 #endif

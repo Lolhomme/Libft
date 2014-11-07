286e38f3af5d71546ba21e9b4ea11ff0df90af73
Laulom anthony <anthonylaulom@gmail.com>
2014-11-07 16:59:54 +0100

    libft

--- libft.h
+++ libft.h
@@ -8,6 +8,7 @@ size_t		ft_strlen(const char *s);
 char		*ft_strcpy(char *dst, const chr *src);
 char		*ft_strncpy(char *dst, char *src, size_t n);
 int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		ft_strcat(char *s1, const char *s2);
 char 		*ft_strdup(const char *s1);
 char		*ft_strchr(const char *s, int c);

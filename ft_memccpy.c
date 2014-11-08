e84626ce6591a4f420d370eab9a2b2b57dac002a
Laulom anthony <anthonylaulom@gmail.com>
2014-11-08 12:47:39 +0100

    memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/08 11:36:23 by alaulom           #+#    #+#             */
+/*   Updated: 2014/11/08 11:56:10 by alaulom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_memccpy(void *dst, const void *src, int c, size_t n)
+{
+	unsigned char			*ptrdst;
+	const unsigned char 	*ptrsrc;
+	unsigned char			cara;
+
+	ptrdst = (unsigned char *)dst;
+	ptrsrc = (const unsigned char *)src;
+	cara = (unsigned char)cara;
+	while (n--)
+		if ((*ptrdst++ = *ptrsrc++) == cara)
+			return (ptrdst);
+	return (NULL);
+}
+

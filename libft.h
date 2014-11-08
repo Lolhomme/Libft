59998e1a0bc7e6708507d802d0477bd8ff5c4b57
Laulom anthony <anthonylaulom@gmail.com>
2014-11-08 18:28:59 +0100

    memchr

--- libft.h
+++ libft.h
@@ -9,6 +9,7 @@ void		ft_bzero(void *s, size_t n);
 void		ft_memcpy(void *dst, const void *src, size_t n);
 void		ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
+void		*ft_memchr(const void *s, int c, size_t n);
 size_t		ft_strlen(const char *s);
 char		*ft_strcpy(char *dst, const chr *src);
 char		*ft_strncpy(char *dst, char *src, size_t n);

4b71ff5bb61d83fa1df7f85be3a9cc0be3e3bbe2
Laulom anthony <anthonylaulom@gmail.com>
2014-11-10 12:17:35 +0100

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/10 11:46:01 by alaulom           #+#    #+#             */
+/*   Updated: 2014/11/10 12:11:01 by alaulom          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int			ft_atoi(const char *str)
+{
+	int		ret;
+	char	sign;
+
+	sign = 1;
+	ret = 0;
+	while (ft_isspace (*str))
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		sign = (*str == '-' ? -1 : 1);
+		str++;
+	}
+	while (ft_isdigit (*str))
+	{
+		ret = ret * 10 + sign * (*str - '0');
+		str++;
+	}
+	return (ret);
+}
--- libft.h
+++ libft.h
@@ -16,6 +16,7 @@ char		*ft_strcpy(char *dst, const chr *src);
 char		*ft_strncpy(char *dst, char *src, size_t n);
 int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
 char		ft_strcat(char *s1, const char *s2);
 char 		*ft_strdup(const char *s1);
 char		*ft_strchr(const char *s, int c);

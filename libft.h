0ed1403fb223c798b9b5ae2b08771f468933e529
Laulom anthony <anthonylaulom@gmail.com>
2014-11-12 11:34:28 +0100

    strmapi

--- libft.h
+++ libft.h
@@ -43,6 +43,7 @@ void		ft_strclr(char *s);
 void		ft_striter(char *s, void (*f)(char *));
 void		ft_striteri(char *s, void (f*)(unsigned int, char *));
 char		*ft_strmap(const char *s, char (*f)(char));
+char		*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void		ft_putchar(char c);
 void		ft_putstr(const char *s);
 void		ft_putendl(const char *s);

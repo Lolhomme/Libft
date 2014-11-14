2ca20efc080c1aae62920d3b4f168829e596734d
Laulom anthony <anthonylaulom@gmail.com>
2014-11-14 16:19:35 +0100

    correction memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,17 +6,17 @@
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/09 10:49:54 by alaulom           #+#    #+#             */
-/*   Updated: 2014/11/14 11:41:32 by alaulom          ###   ########.fr       */
+/*   Updated: 2014/11/14 16:19:11 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int			ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned char		*ptrs1;
-	unsigned char		*ptrs2;
+	const unsigned char		*ptrs1;
+	const unsigned char		*ptrs2;
 
-	ptrs1 = (unsigned char *)s1;
-	ptrs2 = (unsigned char *)s2;
+	ptrs1 = s1;
+	ptrs2 = s2;
 	while (n--)
 	{
 		if (*ptrs1 != *ptrs2)

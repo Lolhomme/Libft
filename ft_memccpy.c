c805143bc0e8dbe8caf9a8589af646797cbdc86b
Laulom anthony <anthonylaulom@gmail.com>
2014-11-14 11:16:41 +0100

    correctifs

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,21 +6,23 @@
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 11:36:23 by alaulom           #+#    #+#             */
-/*   Updated: 2014/11/12 12:15:03 by alaulom          ###   ########.fr       */
+/*   Updated: 2014/11/14 11:15:53 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void					ft_memccpy(void *dst, const void *src, int c, size_t n)
+void					*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char			*ptrdst;
 	const unsigned char		*ptrsrc;
 	unsigned char			cara;
 
+	if (!src || !dst)
+		return (NULL);
 	ptrdst = (unsigned char *)dst;
 	ptrsrc = (const unsigned char *)src;
-	cara = (unsigned char)cara;
+	cara = (unsigned char)c;
 	while (n--)
 		if ((*ptrdst++ = *ptrsrc++) == cara)
 			return (ptrdst);

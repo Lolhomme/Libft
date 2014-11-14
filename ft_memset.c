803aa84d3649d1ec944ae1df0f0b25edd2671ecc
Laulom anthony <anthonylaulom@gmail.com>
2014-11-14 11:00:11 +0100

    correctifs

--- ft_memset.c
+++ ft_memset.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/14 09:40:14 by alaulom           #+#    #+#             */
-/*   Updated: 2014/11/14 09:42:05 by alaulom          ###   ########.fr       */
+/*   Created: 2014/11/14 10:53:34 by alaulom           #+#    #+#             */
+/*   Updated: 2014/11/14 10:56:00 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void		*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*s;
+	unsigned char *s;
 
 	s = (unsigned char *)b;
 	while (len--)

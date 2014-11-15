3f245d243d5fdc0b480e291c943a975831732fcd
Laulom anthony <anthonylaulom@gmail.com>
2014-11-15 19:15:05 +0100

    strsplit

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 17:41:44 by alaulom           #+#    #+#             */
-/*   Updated: 2014/11/08 18:26:43 by alaulom          ###   ########.fr       */
+/*   Updated: 2014/11/14 16:55:42 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void		*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char *ptrs;
+	const unsigned char		*ptrs;
 
 	ptrs = (const unsigned char *)s;
 	while (n--)
 	{
-		if (*ptrs == (unsigned char *)c)
+		if (*ptrs == (unsigned char)c)
 			return ((void *)ptrs);
 		ptrs++;
 	}

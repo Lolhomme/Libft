a14173119e8885873b62251fcb3375a09dd28aa9
Laulom Anthony <anthonylaulom@gmail.com>
2015-01-11 17:26:17 +0100

    f

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: alaulom <anthonylaulom@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 12:58:21 by alaulom           #+#    #+#             */
-/*   Updated: 2015/01/11 16:21:47 by alaulom          ###   ########.fr       */
+/*   Updated: 2015/01/11 17:25:55 by alaulom          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 size_t			ft_strlen(const char *s)
 {
 	size_t		a;
-	
+
 	if (!s)
 		return (0);
 	a = 0;
